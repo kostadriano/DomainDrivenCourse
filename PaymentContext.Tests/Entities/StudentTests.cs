72b592033dfc7b9489834bbcc12446ec06344084
kostadriano <adrianokosta001@gmail.com>
2018-05-08 10:48:54 -0300

    Validations examples

--- PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.ValueObjects;
 
 namespace PaymentContext.Tests
 {
@@ -9,6 +10,12 @@ namespace PaymentContext.Tests
     {
         [TestMethod]
         public void TestMethod(){
+            // var name = new Name("Teste","Teste");
+            // foreach (var not in name.Notifications)
+            // {
+            //     not.Message;
+            //     not.Property;
+            // }
             //var student = new Student("Adriano","Costa","654321","adriano@hotmail.com");
         }
     }
